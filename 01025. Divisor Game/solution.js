f09a4da53969ee7e21d47c9c2cbbafd88fcd2f89
Bagus Valentino <bagusvalentino.dev@gmail.com>
2025-07-09 11:44:45 +0700

    ✨ 💡 Solve LeetCode problem 1025. Divisor Game [Easy] [JavaScript] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 01025. Divisor Game/solution.js
@@ -0,0 +1,18 @@
+/**
+ * Problem: 1025. Divisor Game
+ *
+ * Difficulty: Easy
+ *
+ * Language: JavaScript
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+/**
+ * Determines if the first player wins the Divisor Game
+ *
+ * @param {number} n - Number of stones
+ *
+ * @return {boolean} - True if first player wins
+ */
+const divisorGame = (n) => n % 2 === 0
