043a12ec48788dd4a5c1f671bb98e91a3dbceef5
Bagus Valentino <bagusvalentino.dev@gmail.com>
2025-08-29 20:33:20 +0700

    ✨ 💡 Solve LeetCode problem 3021. Alice and Bob Playing Flower Game [Medium] [JavaScript] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 03021. Alice and Bob Playing Flower Game/solution.js
@@ -0,0 +1,19 @@
+/**
+ * Problem: 3021. Alice and Bob Playing Flower Game
+ *
+ * Difficulty: Medium
+ *
+ * Language: JavaScript
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+/**
+ * Calculates half the product of n and m, rounded down
+ *
+ * @param {number} n - The first number
+ * @param {number} m - The second number
+ *
+ * @returns {number} - The calculated value
+ */
+const flowerGame = (n, m) => Math.floor((n * m) / 2)
