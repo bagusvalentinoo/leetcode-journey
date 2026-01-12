e578299c4711518b3cbb51f809a388a6da69d049
Bagus Valentino <bagusvalentino.dev@gmail.com>
2026-01-12 13:10:28 +0000

    ✨ 💡 Solve LeetCode problem 1266. Minimum Time Visiting All Points [Easy] [C++] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 01266. Minimum Time Visiting All Points/solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * Problem: 1266. Minimum Time Visiting All Points
+ *
+ * Difficulty: Easy
+ *
+ * Language: C++
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int totalTime = 0;
+        
+        // Iterate through each consecutive pair of points
+        for (int i = 1; i < points.size(); i++) {
+            // Calculate x and y coordinate differences
+            int xDiff = abs(points[i][0] - points[i - 1][0]);
+            int yDiff = abs(points[i][1] - points[i - 1][1]);
+            
+            // Add maximum of xDiff and yDiff to total time
+            totalTime += max(xDiff, yDiff);
+        }
+        
+        return totalTime;
+    }
+};
