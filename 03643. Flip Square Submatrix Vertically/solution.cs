d8410c879de01db209c36b52d3dbd1d0458d101f
Bagus Valentino <bagusvalentino.work@gmail.com>
2026-03-21 08:39:45 +0700

    ✨ 💡 Solve LeetCode problem 3643. Flip Square Submatrix Vertically [Easy] [C#] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 03643. Flip Square Submatrix Vertically/solution.cs
@@ -0,0 +1,43 @@
+/**
+ * Problem: 3643. Flip Square Submatrix Vertically
+ *
+ * Difficulty: Easy
+ *
+ * Language: C#
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+public class Solution
+{
+  public int[][] ReverseSubmatrix(int[][] grid, int x, int y, int k)
+  {
+    // Pointer for top row of submatrix
+    int topRow = x;
+    // Pointer for bottom row of submatrix
+    int bottomRow = x + k - 1;
+
+    // Swap rows from top to bottom until they meet
+    while (topRow < bottomRow)
+    {
+      // Swap all columns in current row pair
+      for (int col = y; col < y + k; col++)
+      {
+        // Store current top row value
+        int tempValue = grid[topRow][col];
+
+        // Replace top row with bottom row value
+        grid[topRow][col] = grid[bottomRow][col];
+        // Replace bottom row with stored top row value
+        grid[bottomRow][col] = tempValue;
+      }
+
+      // Move pointers inward
+      topRow++;
+      bottomRow--;
+    }
+
+    // Return grid with reversed submatrix
+    return grid;
+  }
+}
