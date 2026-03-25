5f2a2f5e2bff7773f91d7d824821d8851591d572
Bagus Valentino <bagusvalentino.work@gmail.com>
2026-03-25 12:29:09 +0700

    ✨ 💡 Solve LeetCode problem 48. Rotate Image [Medium] [C++] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 00048. Rotate Image/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Problem: 48. Rotate Image
+ *
+ * Difficulty: Medium
+ *
+ * Language: C++
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+class Solution {
+public:
+  void rotate(vector<vector<int>> &matrix) {
+    // Get matrix size
+    int size = matrix.size();
+
+    // Process each layer from outermost to innermost
+    for (int layer = 0; layer < size / 2; layer++) {
+      // Process each element in current layer
+      for (int offset = layer; offset < size - layer - 1; offset++) {
+        // Store top element temporarily
+        int top = matrix[layer][offset];
+
+        // Move left element to top
+        matrix[layer][offset] = matrix[size - offset - 1][layer];
+        // Move bottom element to left
+        matrix[size - offset - 1][layer] =
+            matrix[size - layer - 1][size - offset - 1];
+        // Move right element to bottom
+        matrix[size - layer - 1][size - offset - 1] =
+            matrix[offset][size - layer - 1];
+        // Move stored top element to right
+        matrix[offset][size - layer - 1] = top;
+      }
+    }
+  }
+};
