bb463bf1e6f197572d68f7424204c089af37dad1
Bagus Valentino <bagusvalentino.work@gmail.com>
2026-04-20 11:09:55 +0700

    ✨ 💡 Solve LeetCode problem 2078. Two Furthest Houses With Different Colors [Easy] [C#] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 02078. Two Furthest Houses With Different Colors/solution.cs
@@ -0,0 +1,30 @@
+/**
+ * Problem: 2078. Two Furthest Houses With Different Colors
+ *
+ * Difficulty: Easy
+ *
+ * Language: C#
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+public class Solution
+{
+  public int MaxDistance(int[] colors)
+  {
+    // Get length of colors array
+    int length = colors.Length;
+
+    // Check from start to find first color that differs from end or start
+    for (int i = 0; i < length; i++)
+    {
+      // If current color differs from last color or first color
+      if ((colors[i] ^ colors[length - 1]) != 0 || (colors[length - 1 - i] ^ colors[0]) != 0)
+        // Return the maximum distance
+        return length - 1 - i;
+    }
+
+    // Fallback return (should not reach here)
+    return 0;
+  }
+}
