9d08df07463681196c446acf9cddc05f7cb0b408
Bagus Valentino <bagusvalentino.work@gmail.com>
2026-05-17 10:23:53 +0700

    ✨ 💡 Solve LeetCode problem 3360. Stone Removal Game [Easy] [C#] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 03360. Stone Removal Game/solution.cs
@@ -0,0 +1,34 @@
+/**
+ * Problem: 3360. Stone Removal Game
+ *
+ * Difficulty: Easy
+ *
+ * Language: C#
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+public class Solution
+{
+  public bool CanAliceWin(int n)
+  {
+    // Counter for number of moves made
+    int moveCount = 0;
+    // Starting pile size to remove
+    int currentPile = 10;
+
+    // Continue while enough stones remain to remove current pile
+    while (n >= currentPile)
+    {
+      // Remove current pile from total stones
+      n -= currentPile;
+      // Decrease pile size for next move
+      currentPile--;
+      // Increment move counter
+      moveCount++;
+    }
+
+    // Alice wins if number of moves is odd (Alice starts first)
+    return moveCount % 2 != 0;
+  }
+}
