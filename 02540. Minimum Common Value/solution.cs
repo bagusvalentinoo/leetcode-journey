d34e4098c46d91811c054e8b15e057727d5888fe
Bagus Valentino <bagusvalentino.work@gmail.com>
2026-05-19 07:26:13 +0700

    ✨ 💡 Solve LeetCode problem 2540. Minimum Common Value [Easy] [C#] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 02540. Minimum Common Value/solution.cs
@@ -0,0 +1,39 @@
+/**
+ * Problem: 2540. Minimum Common Value
+ *
+ * Difficulty: Easy
+ *
+ * Language: C#
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+public class Solution
+{
+  public int GetCommon(int[] nums1, int[] nums2)
+  {
+    // Default answer if no common element found
+    int defaultAnswer = -1;
+
+    // Initialize pointers for both arrays
+    int pointer1 = 0,
+      pointer2 = 0;
+
+    // Iterate while both pointers are within array bounds
+    while (pointer1 < nums1.Length && pointer2 < nums2.Length)
+    {
+      // If elements are equal, return the common element
+      if (nums1[pointer1] == nums2[pointer2])
+        return nums1[pointer1];
+      // If element in nums1 is smaller, move pointer in nums1 forward
+      else if (nums1[pointer1] < nums2[pointer2])
+        pointer1++;
+      // If element in nums2 is smaller, move pointer in nums2 forward
+      else
+        pointer2++;
+    }
+
+    // No common element found
+    return defaultAnswer;
+  }
+}
