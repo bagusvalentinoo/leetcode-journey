48999cb769744a25d5edfada0a88963e3b17fc26
Bagus Valentino <bagusvalentino.work@gmail.com>
2026-06-23 14:54:25 +0700

    ✨ 💡 Solve LeetCode problem 1492. The kth Factor of n [Medium] [C++] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 01492. The kth Factor of n/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Problem: 1492. The kth Factor of n
+ *
+ * Difficulty: Medium
+ *
+ * Language: C++
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+class Solution
+{
+public:
+  int kthFactor(int n, int k)
+  {
+    // Counter for factors found
+    int factorCount = 0;
+
+    // Iterate through all possible factors from 1 to n
+    for (int i = 1; i <= n; i++)
+    {
+      // Check if i is a factor of n
+      if (n % i == 0)
+      {
+        // Increment factor counter
+        factorCount++;
+
+        // If we've found the k-th factor, return it
+        if (factorCount == k)
+          return i;
+      }
+    }
+
+    // Return -1 if k exceeds the total number of factors
+    return -1;
+  }
+};
