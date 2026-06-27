1569a5e7e203b29f4f7a42411432dfa9a401e76e
Bagus Valentino <bagusvalentino.work@gmail.com>
2026-06-27 09:29:11 +0700

    ✨ 💡 Solve LeetCode problem 1037. Valid Boomerang [Easy] [C++] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 01037. Valid Boomerang/solution.cpp
@@ -0,0 +1,21 @@
+/**
+ * Problem: 1037. Valid Boomerang
+ *
+ * Difficulty: Easy
+ *
+ * Language: C++
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+class Solution
+{
+public:
+  bool isBoomerang(vector<vector<int>> &points)
+  {
+    // Check if cross product of vectors is non-zero
+    // (x1 - x2) * (y1 - y3) != (x1 - x3) * (y1 - y2)
+    return (points[0][0] - points[1][0]) * (points[0][1] - points[2][1]) !=
+           (points[0][0] - points[2][0]) * (points[0][1] - points[1][1]);
+  }
+};
