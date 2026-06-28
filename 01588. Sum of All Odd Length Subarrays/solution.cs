9ba1da5d6d6c67e1e12ae2b3e1d6805aa7afb770
Bagus Valentino <bagusvalentino.work@gmail.com>
2026-06-28 13:14:56 +0700

    ✨ 💡 Solve LeetCode problem 1588. Sum of All Odd Length Subarrays [Easy] [C#] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 01588. Sum of All Odd Length Subarrays/solution.cs
@@ -0,0 +1,39 @@
+/**
+ * Problem: 1588. Sum of All Odd Length Subarrays
+ *
+ * Difficulty: Easy
+ *
+ * Language: C#
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+public class Solution
+{
+  public int SumOddLengthSubarrays(int[] arr)
+  {
+    // Initialize result accumulator
+    int totalSum = 0;
+
+    // Get length of input array
+    int arrayLength = arr.Length;
+
+    // Iterate through each element in the array
+    for (int i = 0; i < arrayLength; i++)
+    {
+      // Total number of subarrays containing arr[i]
+      // Formula: (i+1) choices for start * (n-i) choices for end
+      int totalSubarrays = (i + 1) * (arrayLength - i);
+
+      // Number of odd-length subarrays containing arr[i]
+      // For odd lengths, count = ceil(totalSubarrays / 2)
+      int oddSubarrays = (totalSubarrays + 1) / 2;
+
+      // Add contribution of arr[i] to total sum
+      totalSum += arr[i] * oddSubarrays;
+    }
+
+    // Return the total sum of all odd-length subarrays
+    return totalSum;
+  }
+}
