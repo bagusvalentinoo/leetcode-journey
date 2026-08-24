cd38651c1f5cbff4f5c2c37ba6993f3c89284f5f
Bagus Valentino <bagusvalentino.work@gmail.com>
2026-08-24 20:49:18 +0700

    ✨ 💡 Solve LeetCode problem 1332. Remove Palindromic Subsequences [Easy] [C++] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 01332. Remove Palindromic Subsequences/solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem: 1332. Remove Palindromic Subsequences
+ *
+ * Difficulty: Easy
+ *
+ * Language: C++
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+class Solution {
+public:
+  int removePalindromeSub(string s) {
+    // Return 0 if the string is empty
+    if (s.empty())
+      return 0;
+
+    // Two pointers to check if the string is a palindrome
+    int left = 0, right = s.length() - 1;
+
+    // Check if all symmetric characters match
+    while (left < right)
+      if (s[left++] != s[right--])
+        return 2;
+
+    // Return 1 if the string is already a palindrome
+    return 1;
+  }
+};
