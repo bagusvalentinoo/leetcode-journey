b61738b7338bdb4512a704d011bb246ab09bbee6
Bagus Valentino <bagusvalentino.work@gmail.com>
2026-09-15 20:29:01 +0700

    ✨ 💡 Solve LeetCode problem 191. Number of 1 Bits [Easy] [C#] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 00191. Number of 1 Bits/solution.cs
@@ -0,0 +1,31 @@
+/**
+ * Problem: 191. Number of 1 Bits
+ *
+ * Difficulty: Easy
+ *
+ * Language: C#
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+public class Solution
+{
+  public int HammingWeight(int n)
+  {
+    // Counter for set bits found
+    int setBitCount = 0;
+
+    // Clear the lowest set bit each iteration until no bits remain
+    while (n != 0)
+    {
+      // Drop the lowest set bit from n
+      n &= n - 1;
+
+      // Count the removed bit
+      setBitCount++;
+    }
+
+    // Return the total number of set bits
+    return setBitCount;
+  }
+}
