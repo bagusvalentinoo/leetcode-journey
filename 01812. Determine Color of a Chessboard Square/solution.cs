daab8a65c645e470cfeb007ffc57f76f3f57d656
Bagus Valentino <93814776+bagusvalentinoo@users.noreply.github.com>
2026-09-24 08:37:35 +0700

    ✨ 💡 Solve LeetCode problem 1812. Determine Color of a Chessboard Square [Easy] [C#] | Runtime: 0 ms (Beats 100%) & Add explanation comments

---
+++ 01812. Determine Color of a Chessboard Square/solution.cs
@@ -0,0 +1,23 @@
+/**
+ * Problem: 1812. Determine Color of a Chessboard Square
+ *
+ * Difficulty: Easy
+ *
+ * Language: C#
+ *
+ * Performance: Runtime - 0 ms (Beats 100%)
+ */
+
+public class Solution
+{
+  public bool SquareIsWhite(string coordinates)
+  {
+    // Convert file letter to column number (a -> 1, b -> 2, ..., h -> 8)
+    // Convert rank character to row number (1 -> 1, ..., 8 -> 8)
+    int column = coordinates[0] - 'a' + 1, row = coordinates[1] - '0';
+
+    // White squares have odd sum of column and row, black squares have even sum
+    // Return true if sum is odd (white), false if even (black)
+    return (column + row) % 2 == 1;
+  }
+}
